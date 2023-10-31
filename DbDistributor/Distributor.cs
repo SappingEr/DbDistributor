e539eb8f58ad6af5120c8a4584c04c4e1cba46f2
Nikolai Shamshurin <nikolai_shamshurin@gmail.com>
2023-11-01 00:22:37 +0400

    123

--- DbDistributor/Distributor.cs
+++ DbDistributor/Distributor.cs
@@ -2,12 +2,27 @@ namespace DbDistributor;
 
 public class Distributor
 {
-	private readonly DataBase _dataBase;
-
-	public Distributor(DataBase dataBase) 
-		=> _dataBase = dataBase ?? throw new ArgumentNullException(nameof(dataBase));
-
+	private readonly Dictionary<int, DataBase> _dataBases = new();
 	
-	
-	public Task DistributeAsync(Row row) => _dataBase.AddRowAsync(row);
+	public Distributor(IEnumerable<DataBase> dataBases)
+	{
+		if (dataBases is null)
+			throw new ArgumentNullException(nameof(dataBases));
+
+		var count = 1;
+
+		foreach (var dataBase in dataBases)
+		{
+			_dataBases.Add(count, dataBase);
+			count++;
+		}
+	}
+
+
+	public async Task DistributeAsync(Row row)
+	{
+		var dbId = row.ProducerId % _dataBases.Count;
+		var dataBase = _dataBases[dbId];
+		await dataBase.AddRowAsync(row);
+	}
 }

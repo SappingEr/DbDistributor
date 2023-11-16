f64620e6fc343e31fc2ea7a52d3d5ae1189ed063
Nikolai Shamshurin <nikolai_shamshurin@gmail.com>
2023-11-17 00:18:16 +0400

    Implement handle distribution.

--- DbDistributor/Distributor.cs
+++ DbDistributor/Distributor.cs
@@ -1,8 +1,11 @@
+using System.Collections.Concurrent;
+
 namespace DbDistributor;
 
 public class Distributor
 {
-	private readonly Dictionary<int, DataBase> _dataBases = new();
+	private int count;
+	private readonly Dictionary<int, DataBase> dataBases = new();
 	
 	public Distributor(IEnumerable<DataBase> dataBases)
 	{
@@ -13,16 +16,28 @@ public class Distributor
 
 		foreach (var dataBase in dataBases)
 		{
-			_dataBases.Add(count, dataBase);
+			this.dataBases.TryAdd(count, dataBase);
 			count++;
 		}
 	}
-
+	
+	public IEnumerable<DataBase> DataBases => dataBases.Values;
 
 	public async Task DistributeAsync(Row row)
 	{
-		var dbId = row.ProducerId % _dataBases.Count;
-		var dataBase = _dataBases[dbId];
-		await dataBase.AddRowAsync(row);
+		var currentCount = Interlocked.Increment(ref count);
+		
+		switch (currentCount)
+		{
+			case <= 100:
+				await dataBases[1].AddRowAsync(row);
+				break;
+			case <= 200:
+				await dataBases[2].AddRowAsync(row);
+				break;
+			default:
+				await dataBases[3].AddRowAsync(row);
+				break;
+		}
 	}
 }
--- DbDistributorTests/DbDistributorTests.cs
+++ DbDistributorTests/DbDistributorTests.cs
@@ -7,41 +7,49 @@ public class Tests
 	[Test]
 	public async Task Distribute()
 	{
-		var rowsByUserCount = 3;
-		var producersCount = 100;
+		const int rowsByUserCount = 3;
+		const int producersCount = 100;
 		var rowsCount = rowsByUserCount * producersCount;
-		var dataBase = new DataBase();
-		var distributor = new Distributor(dataBase);
+		var dataBases = GetDataBases(3);
+		var distributor = new Distributor(dataBases);
 		var producers = GetProducers(producersCount).ToList();
 		var tasks = new List<Task>();
+		
 		foreach (var producer in producers)
 		{
 			tasks.Add(AddRowsAsync(producer, distributor, rowsByUserCount));
 		}
+
 		await Task.WhenAll(tasks);
-		
-		var resultRowCount = dataBase.RowCount;
-		var resultProducersCount = dataBase.Rows.GroupBy(r=>r.ProducerId).Count();
-		var resultRowPerProducer = dataBase.Rows
-			.GroupBy(r => r.ProducerId).Average(r=>r.Count());
-		var resultGroupByIdCount = dataBase.Rows.GroupBy(r => r.Id).Count();
 
-		Console.WriteLine($"Row count: {dataBase.RowCount}");
-		Console.WriteLine($"Db Id: {dataBase.Id}");
+		var resultRowCount = distributor.DataBases.Sum(db => db.RowCount);
+		var resultProducersCount = distributor.DataBases.SelectMany(db=>db.Rows)
+			.GroupBy(r => r.ProducerId).Count();
+		var resultRowPerProducer = distributor.DataBases.SelectMany(db=>db.Rows)
+			.GroupBy(r => r.ProducerId).Average(r => r.Count());
+		var resultGroupByIdCount = distributor.DataBases.SelectMany(db=>db.Rows).GroupBy(r => r.Id).Count();
 
-		foreach (var row in dataBase.Rows)
+		Console.WriteLine($"All rows count: {resultRowCount}");
+
+		foreach (var dataBase in distributor.DataBases)
 		{
-			Console.WriteLine($"Row {row.Id}:  Producer: {row.ProducerId}   Data: {row.Data}");
+			Console.WriteLine($"DataBase Id: {dataBase.Id}	Rows count: {dataBase.RowCount}");
+			Console.WriteLine($"Db Id: {dataBase.Id}");
+
+			foreach (var row in dataBase.Rows)
+			{
+				Console.WriteLine($"Producer: {row.ProducerId}	Row: {row.Id}	Data: {row.Data}");
+			}
 		}
 
-        Assert.Multiple(() =>
-        {
-            Assert.That(resultRowCount, Is.EqualTo(rowsCount));
-            Assert.That(resultProducersCount, Is.EqualTo(producersCount));
-            Assert.That(resultRowPerProducer, Is.EqualTo(rowsByUserCount));
+		Assert.Multiple(() =>
+		{
+			Assert.That(resultRowCount, Is.EqualTo(rowsCount));
+			Assert.That(resultProducersCount, Is.EqualTo(producersCount));
+			Assert.That(resultRowPerProducer, Is.EqualTo(rowsByUserCount));
 			Assert.That(resultGroupByIdCount, Is.EqualTo(rowsCount));
-        });
-    }
+		});
+	}
 
 	private IEnumerable<Producer> GetProducers(int count)
 	{
@@ -51,9 +59,22 @@ public class Tests
 		{
 			producers[i] = new Producer { Id = i };
 		}
-		
+
 		return producers;
 	}
+
+	private IEnumerable<DataBase> GetDataBases(int count)
+	{
+		var dataBases = new DataBase[count];
+		
+		for (int i = 0; i < count; i++)
+		{
+			dataBases[i] = new DataBase();
+		}
+		
+		return dataBases;
+	}
+
 
 	private async Task AddRowsAsync(Producer producer, Distributor distributor, int count)
 	{
